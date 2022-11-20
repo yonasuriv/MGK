4105dc979a809479153957eb3acb8d3a73513158
Jonathan Di Rico <diricojonathan@gmail.com>
2022-11-20 01:36:05 -0500

    Added Incendiary Bullet File Splitter Script

---
+++ Password Atlas/File Spliter/IncendiaryBullets.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+. ./.shcss.sh
+
+# To make the script work, put your Passwords Text File in the Dictionary Folder
+clear
+echo
+echo "You are about to load MGK with incendiary bullets."
+sleep 3
+echo
+echo " Please have in mind 2 things:"
+echo ""
+echo " 1) $red FILE NAME should be EXACT. $end"
+echo " 2) $red You don't have to write the extension, as for the moment the script ONLY suport .TXT files. $end"
+echo
+sleep 3
+echo " If the file get splitted succesfully, you should see something similar to this:$white xaa$end,$white xab$end,$white xac$end,$white xad$end,$white xae$end...$white xagsr$end,$white xagso$end,$white xagsp$end,$white xagsq$end, etc."
+echo 
+echo "$yellow Enter the name of the file you want split: $end"
+echo
+read fileName
+echo
+echo "$yellow Enter the maximum number of lines that your file should have: $end"
+echo
+read fileLines
+echo
+
+cd Dictionary
+split $fileName.txt -l $fileLines
