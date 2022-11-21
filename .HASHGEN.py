9b58cf9849211f3cbf8972c51abc4b3f6281d6ad
Jonathan Di Rico <diricojonathan@gmail.com>
2022-11-21 10:24:28 -0500

    Added commentaries

--- .HASHGEN.py
+++ .HASHGEN.py
@@ -1,10 +1,40 @@
-# Import HASH Libraries 
+# Importing HASH Libraries 
 import hashlib
 
-password = input("Enter the password you want to generate a HASH: \n")
-# Even that hashlib supports MD5, SHA1, SHA224, SHA256, SHA384, and SHA512, and to make the script faster, since is a beta version
-# I'll be using  SHA256 as an standard rule for most of the encryptions on the internet
+# Asking the user to type the password that want to be hashed
+password = input("Enter the password you want to generate a HASH: \n\n")
+
 # By default, strings in Python are Unicode, meaning you have to encode it into UTF-8
+# Then it will print the password hashed using the method hashdigest, otherwise it will show you a hash object, and that's not what we want
+
+# MD5 HASH
+print()
+hash = hashlib.md5(password.encode("utf-8"))
+print("MD5:      " + hash.hexdigest())
+
+# SHA1 HASH
+print()
+hash = hashlib.sha1(password.encode("utf-8"))
+print("SHA1:     " + hash.hexdigest())
+
+# SHA224 HASH
+print()
+hash = hashlib.sha224(password.encode("utf-8"))
+print("SHA224:   " + hash.hexdigest())
+
+# SHA256 HASH
+print()
 hash = hashlib.sha256(password.encode("utf-8"))
-# Print the password hashed using the method hashdigest, otherwise it will show you a hash object, and that's not what we want
-print(hash.hexdigest())
+print("SHA256:   " + hash.hexdigest())
+
+# SHA384 HASH
+print()
+hash = hashlib.sha384(password.encode("utf-8"))
+print("SHA384:   " + hash.hexdigest())
+
+# SHA512 HASH
+print()
+hash = hashlib.sha512(password.encode("utf-8"))
+print("SHA512:   " + hash.hexdigest())
+
+print()
