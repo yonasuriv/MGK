d438bb706d59762bb1ab88c56b15a4967fcb0689
Jonathan Di Rico <diricojonathan@gmail.com>
2022-11-20 21:06:55 -0500

    Modified output line code

--- .ADDPWD.py
+++ .ADDPWD.py
@@ -1,4 +1,4 @@
 password = input("Enter the password you want add to the Dictionary: \n")
 
-with open('passwords_list.txt', 'a') as f:
+with open('Passwords-List.txt', 'a') as f:
     print(password, file=f)
