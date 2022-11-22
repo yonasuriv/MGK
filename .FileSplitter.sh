907cd6ca7932bd51c8162ca682d33ca919cfcd7f
Jonathan Di Rico <diricojonathan@gmail.com>
2022-11-22 09:16:41 -0500

    Updated File Splitter Script

--- .FileSplitter.sh
+++ .FileSplitter.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 . ./.assets/SHCSS.sh
 
-echo "You are about to load MGK with Incendiary Bullets (Dictionary File Splitter)."
+echo "$green Loading MGK with Incendiary Bullets. $end"
 sleep 3
 echo
 echo " Please have in mind a few things:"
 echo ""
-echo " 1) $green2 Your passwords file should be under the directory 'Passwords Atlas'. $end"
-echo " 1) $green2 When writting the file name, it should be EXACT.. $end"
-echo " 2) $green2 You don't have to write the extension, as for the moment the script ONLY suport .TXT files. $end"
+echo " 1) $green2 Your passwords file should be under the directory 'Wordlists'. $end"
+echo " 1) $green2 When writting the file name, it should be EXACT. $end"
+echo " 2) $green2 When writting the file name, you DON'T have to type the extension, as for the moment, the script ONLY supports .TXT files. $end"
 echo
 sleep 3
-echo " If the file get splitted succesfully, you should see something similar to this:$green xaa$end,$green xab$end,$green xac$end,$green xad$end,$green xae$end...$green xagsr$end,$green xagso$end,$green xagsp$end,$green xagsq$end, etc."
+echo " If the file get splitted succesfully, you should see something similar to this:$green xaa$end,$green xab$end,$green xac$end,$green xad$end,$green xae$end;$green xagsr$end,$green xagso$end,$green xagsp$end,$green xagsq$end..."
 echo 
 echo "$yellow2 Enter the name of the file you want split: $end"
 echo
 read fileName
 echo
-echo "$yellow Enter the maximum number of lines that your file can have (recommended: 5000): $end"
+echo "$yellow2 Enter the maximum number of lines (recommended: 5000): $end"
 echo
 read fileLines
 echo
